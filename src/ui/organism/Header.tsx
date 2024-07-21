a0d7097b5af8e36253fc365367085addfb353f87
JhsK <qwe6293@nate.com>
2024-07-21 16:19:29 +0900

    fix: 간격 및 최대 너비 수정

--- src/ui/organism/Header.tsx
+++ src/ui/organism/Header.tsx
@@ -2,7 +2,7 @@ import Menu from "../molecule/Menu";
 
 function Header() {
   return (
-    <div className="px-10 h-16 flex items-center justify-between border-b border-b-[#EAECF0]">
+    <div className="px-6 h-16 flex items-center justify-between border-b border-b-[#EAECF0]">
       <span>Logo</span>
       <Menu />
       <span>test</span>
