7885354c5d05248e49c7fbe53f5516200e64c253
JhsK <qwe6293@nate.com>
2024-07-28 16:49:27 +0900

    feat: DayPicker Chevron 커스텀 컴포넌트 작성

---
+++ src/ui/DayPicker/Chevron.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { Icon } from "../atom/Icon";
+
+interface IDayPickerChevronProps {
+  className?: string;
+  disabled?: boolean;
+  orientation?: "up" | "down" | "left" | "right";
+  size?: number;
+}
+
+function DayPickerChevron({
+  className,
+  disabled,
+  orientation,
+  size,
+}: IDayPickerChevronProps) {
+  return (
+    <div className={cn("w-full flex items-center justify-between", className)}>
+      {orientation === "left" ? (
+        <Icon id="chevronLeft" size="md" fill="#182230" />
+      ) : (
+        <Icon id="chevronRight" size="md" fill="#182230" />
+      )}
+    </div>
+  );
+}
+
+export default DayPickerChevron;
