621053f2c4dd38c25f99ac247d413d55e770118f
JhsK <qwe6293@nate.com>
2024-08-11 23:49:14 +0900

    feat: 임시 테스트 모달 작성

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { Icon } from "@/ui/atom/Icon";
 import BigCalendar from "@/ui/organism/Calendar";
 import DayPicker from "@/ui/organism/DayPicker";
 import { Sidebar } from "@/ui/organism/Sidebar";
 import CalendarCheckbox from "./components/CalendarCheckbox";
+import { useState } from "react";
+import Modal from "@/ui/organism/Modal";
 
 function CalendarPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSelectSlot = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="flex">
       <Sidebar>
@@ -20,8 +27,19 @@ function CalendarPage() {
         </Sidebar.Body>
       </Sidebar>
       <section className="w-full h-without-header">
-        <BigCalendar />
+        <BigCalendar handleSelectSlot={handleSelectSlot} />
       </section>
+      <Modal show={showModal}>
+        <form className="flex flex-col gap-2">
+          <input type="text" placeholder="제목 입력" />
+          <input type="text" placeholder="장소 입력" />
+          <input type="text" placeholder="메모 입력" />
+          <button type="submit">추가하기</button>
+          <button onClick={() => setShowModal((prev) => !prev)}>
+            취소하기
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 }
