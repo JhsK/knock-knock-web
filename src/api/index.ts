ea3d6a464109c0720a386f4f6002254f4a1016ce
JhsK <qwe6293@nate.com>
2024-08-20 22:32:04 +0900

    feat: ky 최신 버전 설치 및 ky instance token 로직 작성

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,34 @@
 import ky from "@toss/ky";
 
+const DEFAULT_API_RETRY_LIMIT = 3;
 let accessToken: string | null = null;
 
 export const api = ky.create({
-  prefixUrl: "http://localhost:3001",
+  prefixUrl: process.env.NEXT_PUBLIC_API_HOST,
+  retry: {
+    limit: DEFAULT_API_RETRY_LIMIT,
+  },
   hooks: {
     beforeRequest: [
       (request) => {
-        request.headers.set("Authorization", `token ${accessToken}`);
+        request.headers.set("Authorization", `Bearer ${accessToken}`);
+      },
+    ],
+    beforeRetry: [
+      async ({ request, options, error, retryCount }) => {
+        const test = error as any;
+        console.log("error", error);
+
+        if (test !== 401) {
+          return ky.stop;
+        }
+
+        if (retryCount === DEFAULT_API_RETRY_LIMIT - 1) {
+          return ky.stop;
+        }
+
+        const token = await ky("http://localhost:3001/user/refresh");
+        console.log("token", token);
       },
     ],
   },
